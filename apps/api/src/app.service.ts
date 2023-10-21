9c5be74ecf0e9f08799acad0469bcbc8edf94c66
Nehemiah51000 <nehemiahmwangi25@gmail.com>
2023-10-21 11:56:56 +0300

    core(api/App): add test handlers & sevices methods

--- apps/api/src/app.service.ts
+++ apps/api/src/app.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common'
+import { CreateAppDto } from './dtos/create-app.dto'
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!'
+  }
+
+  getHolloPost(content: CreateAppDto) {
+    return {
+      data: content,
+    }
   }
 }
