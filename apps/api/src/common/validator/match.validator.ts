c19f7fbfa9d94f5d0f4afae6b38ed8b1ee580b3d
Nehemiah51000 <nehemiahmwangi25@gmail.com>
2023-10-21 14:24:09 +0300

    feat(api/common): Extended class validator with match validator

---
+++ apps/api/src/common/validator/match.validator.ts
@@ -0,0 +1,36 @@
+import {
+  ValidationArguments,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+  equals,
+  registerDecorator,
+} from 'class-validator'
+import { ValidationOptions, string } from 'joi'
+
+export const Match =
+  <T>(property: keyof T, options?: ValidationOptions) =>
+  (object: unknown, propertyName: string) =>
+    registerDecorator({
+      target: object.constructor,
+      propertyName,
+      options,
+      constraints: [property],
+      validator: MatchConstraint,
+    })
+
+@ValidatorConstraint({ name: 'Match' })
+export class MatchConstraint implements ValidatorConstraintInterface {
+  validate(value: any, validationArguments?: ValidationArguments): boolean {
+    const dto = validationArguments.object
+    const property = validationArguments.constraints.at(0)
+    const compareWithValue = dto[property]
+
+    return equals(compareWithValue, value)
+  }
+
+  defaultMessage(validationArguments?: ValidationArguments): string {
+    const [propertyNameToCompare] = validationArguments.constraints
+
+    return `${validationArguments.property} and ${propertyNameToCompare} does not match`
+  }
+}
