76c15d08b22a0f4c881286f81173e8141a669ef1
Nehemiah51000 <nehemiahmwangi25@gmail.com>
2023-10-23 12:35:53 +0300

    chores(api/dto): added createUsersDto

--- apps/api/src/iam/users/dto/create-user.dto.ts
+++ apps/api/src/iam/users/dto/create-user.dto.ts
@@ -1,1 +1,29 @@
-export class CreateUserDto {}
+import { IsString, IsOptional, IsNotEmpty } from 'class-validator'
+import { Match } from 'src/common/validator/match.validator'
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
+  username: string
+
+  @IsNotEmpty()
+  @IsString()
+  email: string
+
+  @IsNotEmpty()
+  @IsString()
+  password: string
+
+  @IsNotEmpty()
+  @IsString()
+  @Match('password')
+  passwordConfirm: string
+
+  @IsString()
+  @IsOptional()
+  bio: string
+
+  @IsString()
+  @IsOptional()
+  profileImg: string
+}
