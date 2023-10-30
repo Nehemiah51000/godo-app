a634730c80948bbf2f53186ea8e5ab3dfe7ca682
Nehemiah51000 <nehemiahmwangi25@gmail.com>
2023-10-30 10:39:31 +0300

    chore(api/roles): made a roles-response-dto

---
+++ apps/api/src/iam/authorization/roles/dto/roles-response.dto.ts
@@ -0,0 +1,43 @@
+import { ERoles } from 'src/iam/enums/e-roles.enum'
+import { ERoleTypes } from '../enums/e-role-types'
+import { Expose, Type } from 'class-transformer'
+
+export class SlimRolesResponseDto {
+  @Expose()
+  id: string
+
+  @Expose()
+  name: string
+
+  @Expose()
+  isEnabled: boolean
+}
+
+export class RolesResponseDto {
+  @Expose()
+  id: string
+
+  @Expose()
+  name: ERoles
+
+  @Expose()
+  description: string
+
+  @Expose()
+  isEnabled: boolean
+
+  @Expose()
+  @Type(() => SlimRolesResponseDto)
+  assignedFor: SlimRolesResponseDto
+
+  @Expose()
+  type: ERoleTypes
+
+  @Expose()
+  @Type(() => Date)
+  createdAt: Date
+
+  @Expose()
+  @Type(() => Date)
+  updatedAt: Date
+}
