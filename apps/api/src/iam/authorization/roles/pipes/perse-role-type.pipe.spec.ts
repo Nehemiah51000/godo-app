94d5163310ff66e07fd4d386b7434dc2ee8e902d
Nehemiah51000 <nehemiahmwangi25@gmail.com>
2023-10-30 10:41:32 +0300

    chore(api/roles): spec file for the role-type-dto

---
+++ apps/api/src/iam/authorization/roles/pipes/perse-role-type.pipe.spec.ts
@@ -0,0 +1,7 @@
+import { PerseRoleTypePipe } from './perse-role-type.pipe'
+
+describe('PerseRoleTypePipe', () => {
+  it('should be defined', () => {
+    expect(new PerseRoleTypePipe()).toBeDefined()
+  })
+})
