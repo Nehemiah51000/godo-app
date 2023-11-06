b8a107ffce65a5266e74dc1e8ced3db62d63be5d
Nehemiah51000 <nehemiahmwangi25@gmail.com>
2023-11-06 09:17:50 +0300

    feat(api/iam): added configModule and JwtModule to the imports

--- apps/api/src/iam/iam.module.ts
+++ apps/api/src/iam/iam.module.ts
@@ -2,18 +2,29 @@ import { Module } from '@nestjs/common'
 import { UsersModule } from './users/users.module'
 import { HashingService } from './authentication/bcrypt/hashing.service'
 import { BcryptService } from './authentication/bcrypt/bcrypt.service'
-import { AuthController } from './authentication/auth.controller'
+import { AuthController } from './authentication/auth/auth.controller'
 import { FactoryUtils } from 'src/common/services/factory-utils'
 import { RolesModule } from './authorization/roles/roles.module'
-import { AccessesModule } from './authorization/accesses/accesses.module';
+import { AccessesModule } from './authorization/accesses/accesses.module'
+import { JwtModule } from '@nestjs/jwt'
+import { jwtConfigs } from './authentication/configs/jwt.configs'
+import { ConfigModule } from '@nestjs/config'
+import { AuthService } from './authentication/auth/auth.service'
 
 @Module({
-  imports: [UsersModule, RolesModule, AccessesModule],
+  imports: [
+    JwtModule.registerAsync(jwtConfigs.asProvider()),
+    ConfigModule.forFeature(jwtConfigs),
+    UsersModule,
+    RolesModule,
+    AccessesModule,
+  ],
   providers: [
     {
       provide: HashingService,
       useClass: BcryptService,
     },
+    AuthService,
     FactoryUtils,
   ],
   controllers: [AuthController],
