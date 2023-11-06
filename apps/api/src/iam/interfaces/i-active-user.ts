151446bde22390d18ed66f65f18ef37ade065226
Nehemiah51000 <nehemiahmwangi25@gmail.com>
2023-11-06 09:15:36 +0300

    refactor(api/IActiveUser): added fileds baseRole and totalTeamMembers

--- apps/api/src/iam/interfaces/i-active-user.ts
+++ apps/api/src/iam/interfaces/i-active-user.ts
@@ -1,12 +1,26 @@
-import { StringExpressionOperatorReturningArray } from 'mongoose'
-import { ERoles } from '../enums/e-roles.enum'
+import { EPremiumSubscribers, ERoles } from '../enums/e-roles.enum'
 
 export interface IActiveUser {
+  //current user logged into the app
   sub: string
 
-  roles: ERoles
+  //gets the role of the user(regular, admin, standard, premium)
+  role: ERoles
 
   email: string
 
   memberId: string
+
+  /**
+   *  Regardless of who is logged in, the base role MUST always be assigned a value
+   * - for account owners baseRole will be equal to role
+   * - for members baseRole with be equal to accountOwner role
+   */
+  baseRole: EPremiumSubscribers
+
+  /**
+   * auto generates how many peoplea are in a team everytime a member is created
+   * Checks for limits for premium users team users and guest users
+   */
+  totalTeamMembers: number
 }
