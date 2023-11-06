96c66eceb418515c4b37cb743d6e25e9796e011e
Nehemiah51000 <nehemiahmwangi25@gmail.com>
2023-11-06 09:06:45 +0300

    feat(apps/UsersModule): implement auto calculate totalTeamMembers for users during update and adding new user to a team

--- apps/api/src/iam/users/users.module.ts
+++ apps/api/src/iam/users/users.module.ts
@@ -6,6 +6,9 @@ import { User, UserSchema } from './schema/user.schema'
 import { HashingService } from '../authentication/bcrypt/hashing.service'
 import { BcryptService } from '../authentication/bcrypt/bcrypt.service'
 import { FactoryUtils } from 'src/common/services/factory-utils'
+import { TeamsController } from './teams/teams.controller'
+import { TeamsService } from './teams/teams.service'
+import { Team, TeamSchema } from './schema/team.schema'
 
 @Module({
   imports: [
@@ -28,17 +31,71 @@ import { FactoryUtils } from 'src/common/services/factory-utils'
           return schema
         },
       },
+      {
+        name: Team.name,
+        useFactory: (usersService: UsersService) => {
+          const schema = TeamSchema
+
+          schema.statics.calculateTotalMembers = async function (
+            accountOwner: string,
+          ) {
+            //aggrigate number of members
+            const stats = await this.aggregate([
+              //matching the number of member in the database
+              {
+                $match: {
+                  accountOwner,
+                  $and: [{ isActive: true, isResigned: false }],
+                },
+              },
+              //group
+              {
+                $group: {
+                  _id: '$accountOwner',
+                  totalSum: { $sum: 1 },
+                },
+              },
+            ])
+
+            //updating user schema
+
+            if (stats.length > 0) {
+              usersService.update(accountOwner, {
+                totalTeamMembers: stats[0].totalSum,
+              })
+            }
+
+            //calculate totalTeamMembers after creation of the new document
+            schema.post('save', function () {
+              // @ts-expect-error - the method is a custom one
+              this.constructor.calcTotalTeamMembers(this.accountOwner)
+            })
+
+            /// calculate totalTeamMembers after update of document
+            schema.post(/^findOneAnd/, function (doc, next) {
+              doc.constructor.calcTotalTeamMembers(doc.accountOwner)
+
+              return next()
+            })
+          }
+
+          return schema
+        },
+        // inject: [UsersModule],
+        // imports: [UsersService],
+      },
     ]),
   ],
-  controllers: [UsersController],
+  controllers: [UsersController, TeamsController],
   providers: [
     {
       provide: HashingService,
       useClass: BcryptService,
     },
     UsersService,
+    TeamsService,
     FactoryUtils,
   ],
-  exports: [UsersService],
+  exports: [UsersService, TeamsService],
 })
 export class UsersModule {}
