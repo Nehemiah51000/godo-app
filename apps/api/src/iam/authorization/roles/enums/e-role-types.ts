b7f7f30212ba898ae5ccb74ff0789d287fc04d00
Nehemiah51000 <nehemiahmwangi25@gmail.com>
2023-11-06 09:02:54 +0300

    feat(api/iam): implemetns ERolesAdmin & ERolesRegular

--- apps/api/src/iam/authorization/roles/enums/e-role-types.ts
+++ apps/api/src/iam/authorization/roles/enums/e-role-types.ts
@@ -1,4 +1,21 @@
+import { ERoles } from 'src/iam/enums/e-roles.enum'
+
 export enum ERoleTypes {
   REGULAR = 'regular',
   ADMIN = 'admin',
+}
+
+export enum ERolesAdmin {
+  ADMIN = ERoles.ADMIN,
+  ADMIN_MANAGER = ERoles.ADMIN_MANAGER,
+  ADMIN_ASSISTANT = ERoles.ADMIN_ASSISTANT,
+}
+export enum ERolesRegular {
+  TEAM_USER = ERoles.TEAM_USER,
+  PREMIUM_USER = ERoles.PREMIUM_USER,
+  GUEST_USER = ERoles.GUEST_USER,
+  STANDARD_USER = ERoles.STANDARD_USER,
+  FREE_USER = ERoles.FREE_USER,
+  MANAGER = ERoles.MANAGER,
+  MEMBER = ERoles.MEMBER,
 }
