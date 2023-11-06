b055c4642ef7fb95d9fad951d58e1a0b31bb8128
Nehemiah51000 <nehemiahmwangi25@gmail.com>
2023-11-06 09:16:56 +0300

    feat(api/JwtConfigs): Added JwtConfigs for population of responses

---
+++ apps/api/src/iam/authentication/configs/jwt.configs.ts
@@ -0,0 +1,12 @@
+import { registerAs } from '@nestjs/config'
+import { env } from 'node:process'
+
+export const jwtConfigs = registerAs('jwt', () => {
+  return {
+    secret: env.JWT_SECRET,
+    audience: env.JWT_TOKEN_AUDIENCE,
+    issuer: env.JWT_TOKEN_ISSUER,
+    accessTokenTTL: env.JWT_ACCESS_TOKEN_TTL,
+    refreshTokenTTL: env.JWT_REFRESH_TOKEN_TTL,
+  }
+})
