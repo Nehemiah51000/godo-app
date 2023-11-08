88f04d946752b22df788635460404fc2f27ed0b9
Nehemiah51000 <nehemiahmwangi25@gmail.com>
2023-11-08 10:21:56 +0300

    feat(api/iam): implement AuthGuard for api, public, & token guarding

--- apps/api/src/iam/authentication/guards/auth.guard.ts
+++ apps/api/src/iam/authentication/guards/auth.guard.ts
@@ -1,11 +1,62 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common'
-import { Observable } from 'rxjs'
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common'
+import { Reflector } from '@nestjs/core'
+import { AccessTokenGuard } from './access-token.guard'
+import { EAuthTypes } from '../enums/e-auth-types.enum'
+import { AUTH_KEY } from '../decorators/auth.decorator'
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
-    return true
+  private readonly defaultAuthType = EAuthTypes.NONE
+
+  private readonly authTypesGuardMap: Record<
+    EAuthTypes,
+    CanActivate | CanActivate[]
+  > = {
+    [EAuthTypes.BEARER]: this.accessTokenGuard,
+    [EAuthTypes.NONE]: {
+      canActivate: () => Promise.resolve(true),
+    },
+    [EAuthTypes.API]: {
+      canActivate: () => Promise.resolve(true),
+    },
+  }
+
+  constructor(
+    private readonly accessTokenGuard: AccessTokenGuard,
+
+    private readonly reflector: Reflector,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    // extract auth type(s) from the decorator
+    const authTypes = this.reflector.getAllAndOverride<Array<EAuthTypes>>(
+      AUTH_KEY,
+      [context.getHandler(), context.getClass()],
+    ) || [this.defaultAuthType]
+
+    const guards = authTypes.flatMap(
+      authType => this.authTypesGuardMap[authType],
+    )
+
+    let error = new UnauthorizedException()
+
+    for (const instance of guards) {
+      const canActivate = await Promise.resolve(
+        instance.canActivate(context),
+      ).catch(err => {
+        error = err
+      })
+
+      if (canActivate) {
+        return true
+      }
+    }
+
+    throw error
   }
 }
