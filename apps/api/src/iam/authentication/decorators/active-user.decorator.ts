9ad46e60de5ce6b67d0b178a6f4dd814ca6adace
Nehemiah51000 <nehemiahmwangi25@gmail.com>
2023-11-08 10:24:22 +0300

    feat(api/iam): generate access resource

--- apps/api/src/iam/authentication/decorators/active-user.decorator.ts
+++ apps/api/src/iam/authentication/decorators/active-user.decorator.ts
@@ -1,13 +1,13 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common'
 
-import { REQ_ACTIVE_USER } from '../constants/active-user.contant'
+import { ACTIVE_USER_KEY } from '../constants/active-user.contant'
 import { IActiveUser } from 'src/iam/interfaces/i-active-user'
 
 export const ActiveUser = createParamDecorator(
   (_: any, ctx: ExecutionContext): IActiveUser => {
     const req = ctx.switchToHttp().getRequest()
 
-    const activeUserDetails = req[REQ_ACTIVE_USER]
+    const activeUserDetails = req[ACTIVE_USER_KEY]
 
     return activeUserDetails
   },
