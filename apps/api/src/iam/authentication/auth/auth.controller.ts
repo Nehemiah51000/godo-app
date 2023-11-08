5089189a34f2c55e2e4469b897e05008f4ff652b
Nehemiah51000 <nehemiahmwangi25@gmail.com>
2023-11-08 10:18:32 +0300

    chore(api/AuthController): reorder the arrangement of @Auth and @AccessAuth

--- apps/api/src/iam/authentication/auth/auth.controller.ts
+++ apps/api/src/iam/authentication/auth/auth.controller.ts
@@ -8,6 +8,12 @@ import { PerseMongoIdPipe } from 'src/common/pipes/perse-mongo-id.pipe'
 import { IActiveUser } from 'src/iam/interfaces/i-active-user'
 import { ActiveUser } from '../decorators/active-user.decorator'
 import { SwitchedAccountDto } from './dto/switched-accont.dto'
+import { Auth } from '../decorators/auth.decorator'
+import { EAuthTypes } from '../enums/e-auth-types.enum'
+import { AccessAuth } from 'src/iam/authorization/decorators/access-auth.decorator'
+import { EAccessAuthTypes } from 'src/iam/authorization/enums/e-access-auth-types.enum'
+import { RestrictToRole } from 'src/iam/authorization/decorators/restrict-to-role.decorator'
+import { eAllMembersMap, ePremiumSubscribers } from 'src/iam/enums/e-roles.enum'
 
 //http://localhost:4006/v1/auth/signup
 
@@ -31,6 +37,9 @@ export class AuthController {
 
   //@todo: implement Authguard
   @Serialize(SwitchedAccountDto)
+  @RestrictToRole(...ePremiumSubscribers, ...eAllMembersMap)
+  @AccessAuth(EAccessAuthTypes.ROLE)
+  @Auth(EAuthTypes.BEARER)
   @Get('/switch-account/:account-owner-id')
   switchAccount(
     @Param('account-owner-id', PerseMongoIdPipe) accountOwnerId: string,
