e44f9d27c7d74c02e9142d006dac025b27715406
Nehemiah51000 <nehemiahmwangi25@gmail.com>
2023-11-13 07:49:22 +0300

    Feat(api/Todos): define the CreateTodoDto

--- apps/api/src/features/todos/dto/create-todo.dto.ts
+++ apps/api/src/features/todos/dto/create-todo.dto.ts
@@ -1,1 +1,50 @@
-export class CreateTodoDto {}
+import { Type } from 'class-transformer'
+import {
+  IsDate,
+  IsMongoId,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+} from 'class-validator'
+import { Project } from 'src/features/projects/schema/project.schema'
+import { Icon } from 'src/features/icons/schema/icon.schema'
+import { Todo } from '../schema/todo.schema'
+import { EProjectStages } from 'src/features/projects/enums/e-projects-stages.enum'
+
+export class CreateTodoDto {
+  @IsString()
+  @IsNotEmpty()
+  title: string
+
+  @IsOptional()
+  @MaxLength(500)
+  description?: string
+
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
+  progressStage?: string | EProjectStages
+
+  @IsDate()
+  @IsOptional()
+  @Type(() => Date)
+  startAt?: Date
+
+  @IsDate()
+  @IsOptional()
+  @Type(() => Date)
+  endAt?: Date
+
+  @IsMongoId()
+  @IsOptional()
+  projectId?: Project
+
+  @IsMongoId()
+  @IsOptional()
+  iconId?: Icon
+
+  @IsMongoId()
+  @IsOptional()
+  dependsOn?: Todo
+}
