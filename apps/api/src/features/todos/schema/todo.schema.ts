8a49dd44cb9dd900743c51023f935dcdf7764edc
Nehemiah51000 <nehemiahmwangi25@gmail.com>
2023-11-13 07:26:19 +0300

    feat(api/Todos): Define TodosSchema

--- apps/api/src/features/todos/schema/todo.schema.ts
+++ apps/api/src/features/todos/schema/todo.schema.ts
@@ -1,1 +1,70 @@
-export class Todo {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
+import { HydratedDocument, SchemaTypes } from 'mongoose'
+import { Project } from 'src/features/projects/schema/project.schema'
+import { Icon } from 'src/features/icons/schema/icon.schema'
+import { User } from 'src/iam/users/schema/user.schema'
+import { EProjectStages } from 'src/features/projects/enums/e-projects-stages.enum'
+
+@Schema({
+  toJSON: { virtuals: true },
+  toObject: { virtuals: true },
+  timestamps: true,
+})
+export class Todo {
+  @Prop()
+  title: string
+
+  @Prop()
+  description?: string
+
+  @Prop()
+  progressStage?: string | EProjectStages
+
+  @Prop()
+  startAt?: Date
+
+  @Prop()
+  endAt?: Date
+
+  @Prop({
+    type: SchemaTypes.ObjectId,
+    ref: 'User',
+  })
+  userId: User
+
+  @Prop({
+    type: SchemaTypes.ObjectId,
+    ref: 'Project',
+  })
+  projectId?: Project
+
+  @Prop({
+    type: SchemaTypes.ObjectId,
+    ref: 'Project',
+  })
+  iconId?: Icon
+
+  @Prop({
+    default: true,
+  })
+  isEnabled: boolean
+
+  @Prop({
+    type: SchemaTypes.ObjectId,
+    ref: 'Todo',
+  })
+  dependsOn?: Todo
+}
+
+export const TodoSchema = SchemaFactory.createForClass(Todo)
+export type TTodoDoc = HydratedDocument<Todo>
+
+// title: string
+// description?: string
+// progressStage?: string
+// startAt?: Date
+// endAt?: Date
+// userId: User
+// projectId?: Project
+// iconId?: Icon
+// isEnabled: boolean
