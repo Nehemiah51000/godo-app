e729dd247d2b9475b2d37f835da9851d916524c8
Nehemiah51000 <nehemiahmwangi25@gmail.com>
2023-11-13 07:28:37 +0300

    feat(api/Icons): Defined the CreateIcons Dto

--- apps/api/src/features/icons/dto/create-icon.dto.ts
+++ apps/api/src/features/icons/dto/create-icon.dto.ts
@@ -1,1 +1,17 @@
-export class CreateIconDto {}
+import { Transform } from 'class-transformer'
+import { IsNotEmpty, IsOptional, IsString, MaxLength } from 'class-validator'
+
+export class CreateIconDto {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(100)
+  @Transform(({ value }) => value.trim())
+  prettyName: string
+
+  @IsOptional()
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(500)
+  @Transform(({ value }) => value.trim())
+  description?: string
+}
