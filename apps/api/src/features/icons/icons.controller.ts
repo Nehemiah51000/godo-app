e11e5187a61ee1d850de958366e7c66e2f9cfb81
Nehemiah51000 <nehemiahmwangi25@gmail.com>
2023-11-13 07:22:30 +0300

    feat(api/Icons): impliment Icon controllers

--- apps/api/src/features/icons/icons.controller.ts
+++ apps/api/src/features/icons/icons.controller.ts
@@ -1,34 +1,76 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { IconsService } from './icons.service';
-import { CreateIconDto } from './dto/create-icon.dto';
-import { UpdateIconDto } from './dto/update-icon.dto';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+} from '@nestjs/common'
+import { IconsService } from './icons.service'
+import { CreateIconDto } from './dto/create-icon.dto'
+import { UpdateIconDto } from './dto/update-icon.dto'
+import { PerseMongoIdPipe } from 'src/common/pipes/perse-mongo-id.pipe'
+import { ToggleIconsStatusDto } from './dto/toggle-icons-status.dto'
+import { Serialize } from 'src/common/decorators/serialize.decorator'
+import { IconResponseDto } from './dto/icon-response.dto'
+import { AccessAuth } from 'src/iam/authorization/decorators/access-auth.decorator'
+import { EAccessAuthTypes } from 'src/iam/authorization/enums/e-access-auth-types.enum'
+import { Auth } from 'src/iam/authentication/decorators/auth.decorator'
+import { EAuthTypes } from 'src/iam/authentication/enums/e-auth-types.enum'
+import { RestrictToRole } from 'src/iam/authorization/decorators/restrict-to-role.decorator'
+import {
+  EPremiumSubscribers,
+  eAdminMembersMap,
+} from 'src/iam/enums/e-roles.enum'
+import { FilterQuery } from 'mongoose'
+import { Icon } from './schema/icon.schema'
 
-@Controller('icons')
+@Serialize(IconResponseDto)
+@RestrictToRole(EPremiumSubscribers.ADMIN, ...eAdminMembersMap)
+@AccessAuth(EAccessAuthTypes.ROLE)
+@Auth(EAuthTypes.BEARER)
+@Controller({
+  path: 'icons',
+  version: '1',
+})
 export class IconsController {
   constructor(private readonly iconsService: IconsService) {}
 
   @Post()
   create(@Body() createIconDto: CreateIconDto) {
-    return this.iconsService.create(createIconDto);
+    return this.iconsService.create(createIconDto)
   }
 
   @Get()
-  findAll() {
-    return this.iconsService.findAll();
+  findAll(@Query() filters: FilterQuery<Icon>) {
+    return this.iconsService.findAll(filters)
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.iconsService.findOne(+id);
+  @Get(':iconId')
+  findOne(@Param('iconId', PerseMongoIdPipe) iconId: string) {
+    return this.iconsService.findOne(iconId)
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateIconDto: UpdateIconDto) {
-    return this.iconsService.update(+id, updateIconDto);
+  @Patch(':iconId')
+  update(
+    @Param('iconId', PerseMongoIdPipe) iconId: string,
+    @Body() updateIconDto: UpdateIconDto,
+  ) {
+    return this.iconsService.update(iconId, updateIconDto)
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.iconsService.remove(+id);
+  @Patch(':iconId')
+  toggleStatus(
+    @Param('iconId', PerseMongoIdPipe) ProjectId: string,
+    @Body() toggleStatusDto: ToggleIconsStatusDto,
+  ) {
+    return this.iconsService.toggleStatus(ProjectId, toggleStatusDto)
+  }
+
+  @Delete(':iconId')
+  remove(@Param('iconId', PerseMongoIdPipe) iconId: string) {
+    return this.iconsService.remove(iconId)
   }
 }
