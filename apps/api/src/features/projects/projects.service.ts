4f837eb321c94afc39390dfa25c55d0c238418dc
Nehemiah51000 <nehemiahmwangi25@gmail.com>
2023-11-13 11:18:20 +0300

    feat(api/projects): inject Project model

--- apps/api/src/features/projects/projects.service.ts
+++ apps/api/src/features/projects/projects.service.ts
@@ -2,10 +2,21 @@ import { Injectable, Logger, LoggerService } from '@nestjs/common'
 import { CreateProjectDto } from './dto/create-project.dto'
 import { UpdateProjectDto } from './dto/update-project.dto'
 import { ToggleProjectStatusDto } from './dto/toggle-project-status.dto'
+import { InjectModel } from '@nestjs/mongoose'
+import { Project } from './schema/project.schema'
+import { Model } from 'mongoose'
 
 @Injectable()
 export class ProjectsService {
+  private readonly MAX_STANDARD_SUBSCRIBER_PROJECTS = 12
+  private readonly MAX_GUEST_SUBSCRIBER_PROJECTS = 3
+
   private readonly logger: LoggerService = new Logger(ProjectsService.name)
+
+  constructor(
+    @InjectModel(Project.name)
+    private readonly projectModel: Model<Project>,
+  ) {}
 
   create(createProjectDto: CreateProjectDto) {
     return 'This action adds a new Project'
