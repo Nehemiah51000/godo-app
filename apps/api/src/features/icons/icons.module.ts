ddba8745a724a96a52ae4bdfa29a8a83f9da59ba
Nehemiah51000 <nehemiahmwangi25@gmail.com>
2023-11-13 07:23:26 +0300

    feat(api/IconModule): added IconService to the providers

--- apps/api/src/features/icons/icons.module.ts
+++ apps/api/src/features/icons/icons.module.ts
@@ -1,9 +1,25 @@
-import { Module } from '@nestjs/common';
-import { IconsService } from './icons.service';
-import { IconsController } from './icons.controller';
+import { Module } from '@nestjs/common'
+import { IconsService } from './icons.service'
+import { IconsController } from './icons.controller'
+import { MongooseModule } from '@nestjs/mongoose'
+import { Icon, IconSchema } from './schema/icon.schema'
+import { FactoryUtils } from 'src/common/services/factory-utils'
 
 @Module({
+  imports: [
+    MongooseModule.forFeatureAsync([
+      {
+        name: Icon.name,
+        useFactory: () => {
+          const schema = IconSchema
+
+          return schema
+        },
+      },
+    ]),
+  ],
   controllers: [IconsController],
-  providers: [IconsService],
+  providers: [IconsService, FactoryUtils],
+  exports: [IconsService],
 })
 export class IconsModule {}
