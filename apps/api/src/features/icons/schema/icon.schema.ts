e485423a031fc97172567fbd0121ce0553a4e0e6
Nehemiah51000 <nehemiahmwangi25@gmail.com>
2023-11-13 07:25:56 +0300

    feat(api/Icons): define IconsSchema

--- apps/api/src/features/icons/schema/icon.schema.ts
+++ apps/api/src/features/icons/schema/icon.schema.ts
@@ -1,1 +1,35 @@
-export class Icon {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
+import { HydratedDocument } from 'mongoose'
+
+@Schema({
+  toJSON: { virtuals: true },
+  toObject: { virtuals: true },
+  timestamps: true,
+})
+export class Icon {
+  @Prop({
+    unique: true,
+  })
+  slug: string
+
+  @Prop({
+    index: 'text',
+  })
+  prettyName: string
+
+  @Prop()
+  description?: string
+
+  @Prop({
+    default: true,
+    index: true,
+  })
+  isEnabled?: boolean
+}
+
+export const IconSchema = SchemaFactory.createForClass(Icon)
+export type TIconDoc = HydratedDocument<Icon>
+
+//  slug: string
+//  prettyName: string
+//  description?: string
