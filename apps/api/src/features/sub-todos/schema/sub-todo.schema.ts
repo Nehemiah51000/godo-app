7bb2466bca1c2c2c05b0a1a4379e373427546587
Nehemiah51000 <nehemiahmwangi25@gmail.com>
2023-11-13 07:27:00 +0300

    Feat(api/SubTodos): Define SubTodosSchema

--- apps/api/src/features/sub-todos/schema/sub-todo.schema.ts
+++ apps/api/src/features/sub-todos/schema/sub-todo.schema.ts
@@ -1,1 +1,32 @@
-export class SubTodo {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
+import { HydratedDocument, SchemaTypes } from 'mongoose'
+import { Todo } from 'src/features/todos/schema/todo.schema'
+import { User } from 'src/iam/users/schema/user.schema'
+
+@Schema({
+  toJSON: { virtuals: true },
+  toObject: { virtuals: true },
+  timestamps: true,
+})
+export class SubTodo {
+  @Prop({
+    type: SchemaTypes.ObjectId,
+    ref: 'Todo',
+  })
+  todoId: Todo
+
+  @Prop({
+    type: SchemaTypes.ObjectId,
+    ref: 'Todo',
+  })
+  subTodoId: Todo
+
+  @Prop({
+    type: SchemaTypes.ObjectId,
+    ref: 'User',
+  })
+  userId: User
+}
+
+export const SubTodoSchema = SchemaFactory.createForClass(SubTodo)
+export type TSubTodoDoc = HydratedDocument<SubTodo>
