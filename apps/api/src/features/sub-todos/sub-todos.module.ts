9fa2d92f91895a1e55163c39f61955e4d1c6001e
Nehemiah51000 <nehemiahmwangi25@gmail.com>
2023-11-13 07:51:48 +0300

    chore(api/SubTodos): added factoryUtils to the providers and exported SubTodosService

--- apps/api/src/features/sub-todos/sub-todos.module.ts
+++ apps/api/src/features/sub-todos/sub-todos.module.ts
@@ -1,9 +1,25 @@
-import { Module } from '@nestjs/common';
-import { SubTodosService } from './sub-todos.service';
-import { SubTodosController } from './sub-todos.controller';
+import { Module } from '@nestjs/common'
+import { SubTodosService } from './sub-todos.service'
+import { SubTodosController } from './sub-todos.controller'
+import { MongooseModule } from '@nestjs/mongoose'
+import { SubTodo, SubTodoSchema } from './schema/sub-todo.schema'
+import { FactoryUtils } from 'src/common/services/factory-utils'
 
 @Module({
+  imports: [
+    MongooseModule.forFeatureAsync([
+      {
+        name: SubTodo.name,
+        useFactory: () => {
+          const schema = SubTodoSchema
+
+          return schema
+        },
+      },
+    ]),
+  ],
   controllers: [SubTodosController],
-  providers: [SubTodosService],
+  providers: [SubTodosService, FactoryUtils],
+  exports: [SubTodosService],
 })
 export class SubTodosModule {}
