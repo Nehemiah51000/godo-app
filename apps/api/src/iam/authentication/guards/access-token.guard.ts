f8b7abccfcfa044cdb457e67f19319b7a3cde8d8
Nehemiah51000 <nehemiahmwangi25@gmail.com>
2023-11-16 07:36:08 +0300

    fix(api/authentication): naming ids to projects with capitalization

--- apps/api/src/iam/authentication/guards/access-token.guard.ts
+++ apps/api/src/iam/authentication/guards/access-token.guard.ts
@@ -4,49 +4,39 @@ import {
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common'
+import { Request } from 'express'
+import { EAuthTypes } from '../enums/e-auth-types.enum'
 import { JwtService } from '@nestjs/jwt'
 import { AccessesService } from 'src/iam/authorization/accesses/accesses.service'
 import { TAccessResponseDoc } from 'src/iam/authorization/accesses/types/t-access-response-doc.type'
 import { EPremiumSubscribers } from 'src/iam/enums/e-roles.enum'
 import { IActiveUser } from 'src/iam/interfaces/i-active-user'
-import { EAuthTypes } from '../enums/e-auth-types.enum'
 import { ACTIVE_USER_KEY } from '../constants/active-user.contant'
-import { Request } from 'express'
 
 /**
- * -get tokens
- * ways to get tokens
- * 1. from bodies where we generate from headers(use temporarily)
- * 2. cookies --> safest way to estract token(used in production apps)
  *
- * create a method attract token
+ * - tokens
+ *   1. From body extract from headers (use for now - temporarily)
+ *   2. Cookies -> safest (in production apps)
  *
- * verification
- * 1. if user has a token
- *     -if not found throw unauthorized error
- * 2. if the token is valid
+ * -> extract token
+ * -> verify
+ *    - if user has a token
+ *       - if not unauthorized
+ *    - if is a valid token
+ *       - if not unauthorized
  *
- * 3. If token has memberId
+ * -> Find this user from the database (subscriber or a member)
+ *    1. find their accesses [if memberId is in token - we use to find accesses]
+ *       - what is their role
+ *    2. totalMembers
  *
- * after validation find this user from the database(find user as subscriber or member)
- * to find a user you need to find their:
- * 1. accesses(what is their role)(if member a person had a memberId use it to look for accesses)
- * 2. totalMembers they have on their team
- *
- *
- * Attach the user to the request
- * details we need
- * 1. ACTIVE_USER_KEY --> active-user-key
- * get the nestjs request
- * req[key]
- * values
- *   -IActiveUser
- *   - memberId if token has memberId
- *
- *
- * baseRole if token has memberId
- *
- *
+ *    3. Attach user to the request: ACTIVE_USER_KEY -> active-use
+ *      - get the nestjs request
+ *      - req[key]
+ *      - values: IActiveUser
+ *        -  memberId if token has member id
+ *        - baseRole if token has member id, will be for id, token we put in the token
  *
  *
  *
@@ -116,7 +106,9 @@ export class AccessTokenGuard implements CanActivate {
     const baseRole =
       populatedAccessDoc.baseRole as unknown as EPremiumSubscribers
 
-    const { email, totalTeamMembers } = populatedAccessDoc.accountOwner
+    const { email, totalTeamMembers, totalProjects } =
+      populatedAccessDoc.accountOwner
+
     const { name: role } = populatedAccessDoc.roleId
 
     const reqOptions = {
@@ -126,6 +118,7 @@ export class AccessTokenGuard implements CanActivate {
       email,
       role,
       totalTeamMembers,
+      totalProjects,
     } as IActiveUser
 
     req[ACTIVE_USER_KEY] = reqOptions
