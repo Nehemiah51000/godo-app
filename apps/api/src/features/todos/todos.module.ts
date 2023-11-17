ab5fbddce3b9eba6d172dd2a2ac223ed9a8a043b
Nehemiah51000 <nehemiahmwangi25@gmail.com>
2023-11-17 06:43:32 +0300

    chore(api/TodoModule): added the factoryUtils into the providers and injected SubTodosModule and ProjectModule to the imports

--- apps/api/src/features/todos/todos.module.ts
+++ apps/api/src/features/todos/todos.module.ts
@@ -4,7 +4,9 @@ import { TodosController } from './todos.controller'
 import { MongooseModule } from '@nestjs/mongoose'
 import { Todo, TodoSchema } from './schema/todo.schema'
 import { IconsModule } from '../icons/icons.module'
-import { projectsModule } from '../projects/projects.module'
+import { SubTodosModule } from '../sub-todos/sub-todos.module'
+import { ProjectsModule } from '../projects/projects.module'
+import { FactoryUtils } from 'src/common/services/factory-utils'
 
 @Module({
   imports: [
@@ -18,11 +20,11 @@ import { projectsModule } from '../projects/projects.module'
         },
       },
     ]),
+    SubTodosModule,
     IconsModule,
-    projectsModule,
+    ProjectsModule,
   ],
   controllers: [TodosController],
-  providers: [TodosService],
-  exports: [],
+  providers: [TodosService, FactoryUtils],
 })
 export class TodosModule {}
