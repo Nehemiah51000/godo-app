75c9261507f38e7895a2e886ef18ae106f747fed
Nehemiah51000 <nehemiahmwangi25@gmail.com>
2023-11-17 06:44:42 +0300

    feat(api/TodosService): Implimentation of the CRUD in the UsersService

--- apps/api/src/features/todos/todos.service.ts
+++ apps/api/src/features/todos/todos.service.ts
@@ -4,30 +4,102 @@ import { UpdateTodoDto } from './dto/update-todo.dto'
 import { ToggleTodoStatusDto } from './dto/toggle-todo-status.dto'
 import { FreeUpdateTodoDto } from './dto/free-update-todo.dto'
 import { CreateFreeTodoDto } from './dto/create-free-todo.dto'
+import { InjectModel } from '@nestjs/mongoose'
+import { Todo } from './schema/todo.schema'
+import { FilterQuery, Model, PopulateOptions } from 'mongoose'
+import { SubTodosService } from '../sub-todos/sub-todos.service'
+import { IconsService } from '../icons/icons.service'
+import { IActiveUser } from 'src/iam/interfaces/i-active-user'
+import { ProjectsService } from '../projects/projects.service'
+import { FactoryUtils } from 'src/common/services/factory-utils'
 
 @Injectable()
 export class TodosService {
   private readonly logger: LoggerService = new Logger(TodosService.name)
 
-  createFree(createTodoDto: CreateFreeTodoDto) {
-    return 'This action adds a new todo'
+  private readonly MAX_STANDARD_SUBSCRIBER_PROJECTS = 12
+  private readonly MAX_GUEST_SUBSCRIBER_PROJECTS = 6
+
+  constructor(
+    @InjectModel(Todo.name)
+    private readonly todoModel: Model<Todo>,
+
+    private readonly subTodosService: SubTodosService,
+
+    private readonly iconsService: IconsService,
+
+    private readonly projectService: ProjectsService,
+
+    private readonly factoryUtils: FactoryUtils,
+  ) {}
+
+  async createFree(
+    createTodoDto: CreateFreeTodoDto | CreateTodoDto,
+    activeUser: IActiveUser,
+  ) {
+    const parentId = createTodoDto?.parentId
+    const hasParentOrSubParentId = !!parentId
+
+    // remove parentId & subParentId
+    delete createTodoDto?.parentId
+
+    const createdTodo = await this.todoModel.create(createTodoDto)
+
+    const todoId = createdTodo.id
+
+    if (hasParentOrSubParentId) {
+      const parentId = createTodoDto?.parentId
+      const subParentId = createTodoDto?.subParentId
+      // create sub-todo
+      await this.subTodosService.create(
+        {
+          subTodo: todoId,
+          parentId,
+          ...(subParentId ? { subParentId } : {}),
+        },
+        activeUser,
+      )
+    }
+
+    return {
+      message: `A new todo was added successfully`,
+      data: createTodoDto,
+    }
   }
 
-  create(createTodoDto: CreateTodoDto) {
-    return 'This action adds a new todo'
+  async create(createTodoDto: CreateTodoDto, activeUser: IActiveUser) {
+    // @TODO: before save - check if the project exists & belongs to user
+    if (createTodoDto?.projectId) {
+      // process data
+      // response
+    }
+
+    // assign
+    return this.createFree(createTodoDto, activeUser)
   }
 
-  findAll() {
-    return `This action returns all todos`
+  async findAll(filters: FilterQuery<Todo>, activeUser: IActiveUser) {
+    const todos = await this.todoModel
+      .find({
+        userId: activeUser.sub,
+        ...filters,
+      })
+      .sort('-createdAt')
+      .populate(this.populateConfigs())
+
+    return {
+      data: todos,
+    }
   }
 
-  findOne(todoId: string) {
+  findOne(todoId: string, activeUser: IActiveUser) {
     return `This action returns a #${todoId} todo`
   }
 
   update(
     todoId: string,
     updateTodoDto: Partial<UpdateTodoDto | ToggleTodoStatusDto>,
+    activeUser: IActiveUser,
   ) {
     return `This action updates a #${todoId} todo`
   }
@@ -35,15 +107,43 @@ export class TodosService {
   updateFree(
     todoId: string,
     updateTodoDto: Partial<FreeUpdateTodoDto | ToggleTodoStatusDto>,
+    activeUser: IActiveUser,
   ) {
     return `This action updates a #${todoId} todo`
   }
 
-  toggleStatus(todoId: string, toggleStatusDto: ToggleTodoStatusDto) {
+  toggleStatus(
+    todoId: string,
+    toggleStatusDto: ToggleTodoStatusDto,
+    activeUser: IActiveUser,
+  ) {
     return `toggleStatus`
   }
 
-  remove(todoId: string) {
+  remove(todoId: string, activeUser: IActiveUser) {
     return `This action removes a #${todoId} todo`
+  }
+
+  /**
+   * ---------------------------------------
+   *
+   *                 HELPERS
+   *
+   * ---------------------------------------
+   *
+   */
+
+  /**
+   * Pre configure populate fields
+   * @returns
+   */
+  private populateConfigs(): PopulateOptions[] {
+    return [
+      { path: 'userId' },
+      { path: 'projectId' },
+      { path: 'parentTodos.parentId' },
+      { path: 'parentTodos.subParentId' },
+      { path: 'iconId' },
+    ]
   }
 }
